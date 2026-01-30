195bc751f703df80e537a5f82e74d679416928d1
Eshratjahan17 <eshratjahan759@gmail.com>
2026-01-30 08:07:26 +0600

    feature catagory added

---
+++ src/Components/HomeCat/index.jsx
@@ -0,0 +1,57 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { useState } from 'react';
+const HomeCat =()=>{
+    const [itemBg, setItemBg]=useState([
+       '#fffceb',
+  '#f1f7f3',
+  '#feefea',
+  '#f4fbf8',
+  '#fff3e6',
+  '#ecffec',
+  '#fff1f3',
+  '#fdf1e7',
+  '#eef6f4',
+  '#fff8f0',
+  '#f5f3f0',
+  '#ecffec',
+  '#fff3e6',
+  '#f1f7f3',
+  '#feefea',
+  '#fef6e9',
+  '#fffceb'
+    ])
+    return <section className="homeCat">
+      <div className=''>
+      <h3 className=" hd">FEATURE CATAGORIES</h3>
+      <Swiper slidesPerView={10}
+       spaceBetween={20}
+        navigation={false} 
+        slidesPerGroup={1}
+        centeredSlides={false}
+        pagination={ true }
+        modules={[Pagination]} className="mySwiper ">
+             {
+                itemBg?.map((item,index)=>{
+                    return <SwiperSlide>
+       <div className="item text-center cursor-auto" style={{background:item}}>
+        <img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" alt="" />
+        <h4>Cloths</h4>
+        </div>
+       </SwiperSlide>
+                })
+             }            
+       
+     
+        </Swiper>
+      </div>
+       
+    </section>
+}
+
+export default HomeCat;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,24 +1,30 @@
 import Button from "@mui/material/Button";
 import HomeBanner from "../../Components/HomeBanner";
 import banner1 from "../../assets/Images/banner1.jpg";
+import banner2 from "../../assets/Images/banner2.jpg";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import Rating from '@mui/material/Rating';
 import { Navigation } from 'swiper/modules';
 import ProductItem from "../../Components/ProductItem/ProductItem";
+import HomeCat from "../../Components/HomeCat";
 
 const Home = () => {
   
-    return <div>
+    return <>
+       
     <HomeBanner></HomeBanner>
+    <HomeCat></HomeCat>
     <section className="homeProducts">
         <div className="container" >
             <div className="flex items-start justify-center gap-4">
                 <div className="w-1/4">
                     <div className="banner">
                         <img src={banner1} alt="" className="cursor" />
+                    </div>
+                    <div className="banner mt-4">
+                        <img src={banner2} alt="" className="cursor" />
                     </div>
                 </div>
 
@@ -27,6 +33,60 @@ const Home = () => {
                         <div className="info">
                             <h3 className="mb-0 hd">BEST SELLERS</h3>
                             <p className="text-light text-sml ">Do not miss the current offers until the end of March.
+
+                            </p>
+                           
+                        </div>
+                       
+                        <Button className="viewAllbtn " sx={{ ml: 'auto' }}>View All<IoIosArrowRoundForward /></Button>
+
+                       
+                      </div>
+                      <div className="product_row  mt-4">
+                     
+                    <Swiper slidesPerView={4}
+                    spaceBetween={0}
+                    navigation={true} 
+                    slidesPerGroup={1}
+                    modules={[Navigation]} className="mySwiper">
+                         
+                            <SwiperSlide>
+                                
+                               <ProductItem></ProductItem>
+                            
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <ProductItem></ProductItem>
+                            
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <ProductItem></ProductItem>
+                            </SwiperSlide>
+                            
+                            <SwiperSlide>
+                            <ProductItem></ProductItem>
+                            
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <ProductItem></ProductItem>
+                            
+                            </SwiperSlide>
+                            <SwiperSlide>
+                            <ProductItem></ProductItem>
+                            </SwiperSlide>
+                           
+                           
+                         </Swiper>
+
+                      </div>
+
+
+                      <div className="flex items-center ">
+                        <div className="info">
+                            <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                            <p className="text-light text-sml ">New products with updated stocks.
+
+
 
                             </p>
                            
@@ -76,6 +136,6 @@ const Home = () => {
             </div>
         </div>
     </section>
-    </div>;
+    </>;
 }
 export default Home;
